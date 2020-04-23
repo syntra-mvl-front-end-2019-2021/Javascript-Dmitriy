4327f12eaf85bd7473f3d0ab728a2d45475ada0a
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2020-04-23 21:25:01 +0200

    init

---
+++ les 1/script.js
@@ -0,0 +1,5 @@
+    //    alert("hello world");
+    let name = prompt("wat is je naam?");
+    console.log(name);
+    let divText = "je naam is " + name;
+    document.getElementById('cool-div').innerHTML = divText;
