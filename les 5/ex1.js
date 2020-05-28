b904f58c3bff2500e9602591db538b22f36823c2
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2020-05-28 18:38:05 +0200

    j

--- les 5/ex1.js
+++ les 5/ex1.js
@@ -1,14 +1,13 @@
+11 lines (9 sloc)  361 Bytes
+  
+let par1 = document.getElementById('p-1');
+let par2 = document.getElementById('p-2');
+let par3 = document.getElementById('p-3');
 
-let par1=  document.getElementById('p-1')
-let par2= document.getElementById('p-2')
-let par3=document.getElementById('p-3')
-
-function paragraphClicked(event){
-  console.log(event)
-  event.target.innertext = 'hello world'
+function paragraphClicked(event) {
+    event.target.innerText = 'Hello World';
 }
 
-par1.addEventListener('click', paragraphClicked)
-par2.addEventListener('click', paragraphClicked)
-par3.addEventListener('click', paragraphClicked)
-
+par1.addEventListener('click', paragraphClicked);
+par2.addEventListener('click', paragraphClicked);
+par3.addEventListener('click', paragraphClicked);
---
+++ les 5/ex2.js
@@ -0,0 +1,11 @@
+const inputField = document.getElementById('input');
+const button = document.getElementById('btn');
+const resultPar = document.getElementById('result');
+
+console.log(inputField.value);
+
+function printInputValue(event) {
+    resultPar.innerText = inputField.value;
+}
+
+button.addEventListener('click', printInputValue);
