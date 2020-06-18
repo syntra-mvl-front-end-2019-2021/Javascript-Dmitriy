c4e36c408a54adbf23910c2c3fa7a1c5aec6072c
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2020-06-18 23:04:38 +0200

    lll

--- les 7/script.js
+++ les 7/script.js
@@ -1,92 +1,60 @@
 
-// let wordsToScramble = document.getElementById("user-input");
-// console.log(wordsToScramble.innerHTML);
-
-// let wordsToScrambleArray = wordsToScramble.value.split(' ');
-// console.log(wordsToScrambleArray);
-// let randomArray = [];
-
-// let scramble = document.getElementById('submit-btn');
-// let scrambleContainer = document.getElementById("result-container");
-
-
-//  function putStuffIn() 
-//  {
-//   scrambleContainer.textContent = `${event.target.value}`;
-//  }
-
-
-
-
-
-// function scrambleYoStuff() {
-//   for (i = 0; i < wordsToScrambleArray.length; i++) {
-//     let random =
-//     wordsToScrambleArray[
-//         Math.floor(Math.random() * wordsToScrambleArray.length)
-//       ];
-//     if (randomArray.indexOf(random) == -1) {
-//       randomArray.push(random);
-//     } else i--;
-    
-//   }
-//   scrambleContainer.textContent = randomArray.join(' ')
- 
-
-// }
-
-
-
-// wordsToScramble.addEventListener("change",putStuffIn);
-// // scramble.addEventListener('click', scrambleYoStuff);
-
-
 let wordsToScramble = document.getElementById("user-input");
-console.log(wordsToScramble.innerHTML);
-
-let wordsToScrambleArray = [];
-console.log( wordsToScrambleArray);
-
-x = wordsToScrambleArray.toString('')
-y = x.split(' ')
-console.log(y);
-
-
-
-
-
-
-let randomArray = [];
-
 let scramble = document.getElementById('submit-btn');
 let scrambleContainer = document.getElementById("result-container");
 
+let wordCountContainer = document.getElementById('word-count');
+let letterCountContainer = document.getElementById('letter-count');
 
- function putStuffIn() 
- {
 
-  wordsToScrambleArray.push(wordsToScramble.textContent = `${event.target.value}`)
-  
- }
+function string(){
+
+  return  wordsToScramble.value;
+
+}
+
+txt = string()
+
+function x(txt){
+  return scrambleContainer.innerText   = txt
+}
+e = x(txt)
+function y(e){
+  return  e.split(' ')
+}
+wordsToScrambleArray = y(e)
+
+
+console.log(wordsToScrambleArray)
+
+
+
+
+
 
 
 function scrambleYoStuff() {
+  
+  let randomArray = [];
+
   for (i = 0; i < wordsToScrambleArray.length; i++) {
-    let random =
-    wordsToScrambleArray[
-        Math.floor(Math.random() * wordsToScrambleArray.length)
-      ];
-    if (randomArray.indexOf(random) == -1) {
+    let random = wordsToScrambleArray[
+              Math.floor(Math.random() * wordsToScrambleArray.length)
+            ];
+        if (randomArray.indexOf(random) == -1) {
       randomArray.push(random);
     } else i--;
-    
-  }
-  scrambleContainer.textContent = randomArray.join(' ')
+  } 
  
 
+ 
 }
 
 
 
-wordsToScramble.addEventListener("change",putStuffIn);
-scramble.addEventListener('click', scrambleYoStuff);
+
+function realtime() {
+  x(txt)  
+}
+
+wordsToScramble.addEventListener('input', realtime);
