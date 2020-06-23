43df49e4b20695cebdf070c9253a0d95cdb09539
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2020-06-23 20:40:24 +0200

    hangInThereMan

--- les 7/script.js
+++ les 7/script.js
@@ -1,60 +1,91 @@
 
-let wordsToScramble = document.getElementById("user-input");
-let scramble = document.getElementById('submit-btn');
-let scrambleContainer = document.getElementById("result-container");
+// let wordsToScramble = document.getElementById("user-input");
+// let scramble = document.getElementById('submit-btn');
+// let scrambleContainer = document.getElementById("result-container");
 
-let wordCountContainer = document.getElementById('word-count');
-let letterCountContainer = document.getElementById('letter-count');
-
-
-function string(){
-
-  return  wordsToScramble.value;
-
-}
-
-txt = string()
-
-function x(txt){
-  return scrambleContainer.innerText   = txt
-}
-e = x(txt)
-function y(e){
-  return  e.split(' ')
-}
-wordsToScrambleArray = y(e)
+// let wordCountContainer = document.getElementById('word-count');
+// let letterCountContainer = document.getElementById('letter-count');
 
 
-console.log(wordsToScrambleArray)
+// function string(){
+
+//   return  wordsToScramble.value;
+
+// }
+
+// txt = string()
+
+// function x(txt){
+//   return scrambleContainer.innerText   = txt
+// }
+// e = x(txt)
+// function y(e){
+//   return  e.split(' ')
+// }
+// wordsToScrambleArray = y(e)
 
 
+// console.log(wordsToScrambleArray)
 
 
 
 
 
-function scrambleYoStuff() {
+
+
+// function scrambleYoStuff() {
   
-  let randomArray = [];
+//   let randomArray = [];
 
-  for (i = 0; i < wordsToScrambleArray.length; i++) {
-    let random = wordsToScrambleArray[
-              Math.floor(Math.random() * wordsToScrambleArray.length)
-            ];
-        if (randomArray.indexOf(random) == -1) {
-      randomArray.push(random);
-    } else i--;
-  } 
+//   for (i = 0; i < wordsToScrambleArray.length; i++) {
+//     let random = wordsToScrambleArray[
+//               Math.floor(Math.random() * wordsToScrambleArray.length)
+//             ];
+//         if (randomArray.indexOf(random) == -1) {
+//       randomArray.push(random);
+//     } else i--;
+//   } 
  
 
  
-}
+// }
 
 
 
 
-function realtime() {
-  x(txt)  
-}
+// function realtime() {
+//   x(txt)  
+// }
 
-wordsToScramble.addEventListener('input', realtime);
+// wordsToScramble.addEventListener('input', realtime);
+
+
+
+
+// var txtWord = document.getElementById("word");
+// var btn = document.getElementById("submit");
+
+
+// function random() {
+
+//   // Separate all the letters into an array
+//   var letterArray = txtWord.value.split("");
+  
+//   // create an new array from scrambled letters in the original array
+//   var scrambledArray = [];
+  
+//   var len = letterArray.length;
+//   for (var start = 0; start < len; start++) {
+//     // Get a random number between 0 and the highest array index and put it in the new array
+//     var num = Math.floor(Math.random() * letterArray.length)
+//     scrambledArray.push(letterArray[num]);
+    
+//     // Remove letter from original array
+//     letterArray.splice(num,1);
+//   }
+//   console.log(scrambledArray.join(""));
+// }
+
+// btn.addEventListener("click", random);
+
+
--- les 8/script.js
+++ les 8/script.js
@@ -50,10 +50,15 @@ const randomWords = [
   'other',
   'prospect',
 ];
+
+
+
 const hangManImage = document.querySelector('#image');
 const solutionContainer = document.querySelector('#solution-container');
 const winOrLoseContainer = document.querySelector('#win-lose-container');
 const letterContainer = document.querySelector('#letter-container');
+
+
 const gameState = {
   word: [],
   hangman: 1,
@@ -62,3 +67,53 @@ const gameState = {
   won: false,
   lost: false,
 };
+
+function initGame() {
+  // choose a random word > save in gamestate
+  for (i = 0; i < randomWords.length; i++) {
+         gameState.word = randomWords[
+                  Math.floor(Math.random() * randomWords.length)
+                ];
+
+              }
+console.log(gameState.word)
+  // reset gamestate
+
+  gameState = {
+    word: [],
+    hangman: 1,
+    turn: 1,
+    lettersFound: 0,
+    won: false,
+    lost: false,
+  }
+
+  // update solutionContainer > insert div.solution-letter per letter
+
+  
+
+
+  // empty winOrLoseContainer.innerHTML
+  winOrLoseContainer.innerHTML = " "
+  // remove success and failed classes from .letter
+
+  
+let letter = document.querySelectorAll('.letter');
+
+letter.forEach(function(el) {
+  el.classList.remove('success', 'failed');
+});
+
+
+            }
+
+function clickLetter(event) {
+  // check if event.target is .letter
+  // check if event.target has success or failed class
+  // ---
+}
+
+// hangManImage.setAttribute('src', 'images/hangman02.png');
+
+
+
